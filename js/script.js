61bd46225b2e3bdba7bf29be76228de5d584d580
nik <spiritofice0@gmail.com>
2021-12-04 05:47:13 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+"use strict";
+//alert('hello');
+//const rezult = confirm('шо робишь?');
+//if (rezult == true)
+//{document.write(rezult);}
+///else document.write('ниче');
+console.log("arr"+"-object");
+const isCheck = true,
+isClose = false;
+console.log(isCheck&&isClose);
